bbada6738cddf5ca6176a68d1fa59f5614378e67
Spencer Oberstadt <soberstadt@gmail.com>
2019-05-10 11:52:19 -0400

    exclude telebears from cowsay

--- oh-my-zsh/custom/randomsay.zsh
+++ oh-my-zsh/custom/randomsay.zsh
@@ -1,4 +1,4 @@
 randomsay() {
-  cow=(`cowsay -l | tail -n +2 | tr  " "  "\n" | sort -R | head -n 1`)
+  cow=(`cowsay -l | tail -n +2 | tr  " "  "\n" | egrep -v 'telebears' | sort -R | head -n 1`)
   cowsay -f $cow "$@"
 }
