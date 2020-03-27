1b34b6a74697508913c3472432ef836de3ac6cd8
Spencer Oberstadt <soberstadt@gmail.com>
2020-03-26 22:13:05 -0400

    remove other gross cowsay

--- oh-my-zsh/custom/randomsay.zsh
+++ oh-my-zsh/custom/randomsay.zsh
@@ -1,4 +1,4 @@
 randomsay() {
-  cow=(`cowsay -l | tail -n +2 | tr  " "  "\n" | egrep -v 'telebears' | sort -R | head -n 1`)
+  cow=(`cowsay -l | tail -n +2 | tr  " "  "\n" | egrep -v 'telebears|sodomized' | sort -R | head -n 1`)
   cowsay -f $cow "$@"
 }
