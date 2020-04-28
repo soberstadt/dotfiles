0a61239f16770d545c928ba1e7d60b7fd09c0082
Spencer Oberstadt <soberstadt@gmail.com>
2020-04-27 21:33:39 -0400

    clean up docker ssh function

--- oh-my-zsh/custom/docker-ssh.zsh
+++ oh-my-zsh/custom/docker-ssh.zsh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+# ssh into the only or specified container
 docker-ssh() {
+  # check if there is any args
   if [[ $# -gt 0 ]]; then
-    docker exec -it "$@" /bin/bash
+    # if so, use the first arg as the container id
+    id=$1
   else
-    if [[ $(docker ps | wc -l) -eq 2 ]]; then
-      docker exec -it "$(docker ps | awk 'FNR == 2 {print $1}')" /bin/bash
-    else
+    # otherwise, check if there is exactly one container running
+    if [[ $(docker ps | wc -l) -ne 2 ]]; then
+      # if there is not, exit because we don't know which to use
       echo "There is not only one docker container running. Please specify one."
+      return 1
     fi
+    # grab the first container id
+    id=$(docker ps | awk 'FNR == 2 {print $1}')
   fi
+
+  # run bash in the container
+  docker exec -it "$id" /bin/bash
 }
